d562bbae262ab9c2f0b10de441aa64341f9b9b7c
charliee.cy <carloscy8@gmail.com>
2025-05-03 18:05:22 +0200

    Estados de jugador y entrenador

--- src/main/kotlin/org/example/newteam/gestion/viewmodels/EquipoViewModel.kt
+++ src/main/kotlin/org/example/newteam/gestion/viewmodels/EquipoViewModel.kt
@@ -1,9 +1,40 @@
 package org.example.newteam.gestion.viewmodels
 
 import org.example.newteam.gestion.service.EquipoService
+import java.time.LocalDate
 
 class EquipoViewModel (
     val service: EquipoService
 ) {
 
+    data class EntrenadorState(
+        val id: Long = 0L,
+        val nombre: String = "",
+        val apellidos: String = "",
+        val fecha_nacimiento: LocalDate = LocalDate.now(),
+        val fecha_incorporacion: LocalDate = LocalDate.now(),
+        val salario: Double = 0.0,
+        val pais: String = "",
+        val especialidad: String = "",
+        val imagen: String = ""
+    )
+
+    data class JugadorState(
+        val id: Long = 0L,
+        val nombre: String = "",
+        val apellidos: String = "",
+        val fecha_nacimiento: LocalDate = LocalDate.now(),
+        val fecha_incorporacion: LocalDate = LocalDate.now(),
+        val salario: Double = 0.0,
+        val pais: String = "",
+        val imagen: String = "",
+        val posicion: String = "",
+        val dorsal: Int = 99,
+        val altura: Double = 0.0,
+        val peso: Double = 0.0,
+        val goles: Int = 0,
+        val partidos_jugados: Int = 0,
+        val minutos_jugados: Int = 0
+
+    )
 }
