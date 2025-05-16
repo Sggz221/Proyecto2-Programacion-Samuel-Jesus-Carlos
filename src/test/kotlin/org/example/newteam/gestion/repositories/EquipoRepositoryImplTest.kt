7f7af625509730fbc814b3e461171b69c02cf4c8
Samuel <sggz221@gmail.com>
2025-05-16 17:18:31 +0200

    Fallo en la vista de Admin arreglado

---
+++ src/test/kotlin/org/example/newteam/gestion/repositories/EquipoRepositoryImplTest.kt
@@ -0,0 +1,12 @@
+package org.example.newteam.gestion.repositories
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class EquipoRepositoryImplTest{
+
+    @Test
+    fun prueba(){
+
+    }
+}
